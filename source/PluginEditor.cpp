f417e98a2a230f8d7385e098b0a2eb020ce4ba48
Hippasusss <dannyherbertgj@gmail.com>
2025-06-17 23:17:58 +0100

    moved bounds setting to resizw

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -27,13 +27,13 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-    auto bounds = getLocalBounds();
-    titleBar.setBounds(bounds.removeFromTop(100));
-    mainSlider.setBounds(bounds);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
+    auto bounds = getLocalBounds();
+    titleBar.setBounds(bounds.removeFromTop(100));
+    mainSlider.setBounds(bounds);
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
 }
